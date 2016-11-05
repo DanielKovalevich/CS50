f177fd502b64f905b687b386d8f24bf0103dde9f
Dkoval526 <dkoval526@gmail.com>
2016-11-05 13:38:26 -0400

    Add files via upload

--- pset4/jpg/recover.c
+++ pset4/jpg/recover.c
@@ -10,18 +10,19 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <stdbool.h>
+ #include <stdint.h>
  
  const char* FILE_NAME = "card.raw";
  const int MAX_JPEG = 50;
  const int BLOCK_SIZE = 512;
- const int FIRST_BYTE = 0xff,
-           SECOND_BYTE = 0xd8,
-           THIRD_BYTE = 0xff;
+ const uint8_t FIRST_BYTE = 0xff,
+               SECOND_BYTE = 0xd8,
+               THIRD_BYTE = 0xff;
 
 int main(int argc, char* argv[])
 {
     // Ensure proper usage
-    if (argc != 1)
+    if (argc != 1 && argv[0] != NULL)
     {
         printf("Usage: ./recover\n");
         return 1;
@@ -36,31 +37,22 @@ int main(int argc, char* argv[])
     }
     
     int currentJPEG = 0;
+    bool startJPG = false;
+    uint8_t buffer[BLOCK_SIZE];
     
-    int *buffer;
-    buffer = (int *) malloc(BLOCK_SIZE);
+    // Output file object
+    FILE* img = NULL;
     
     while (currentJPEG < MAX_JPEG){
+        bool newJPG = false;
         
-        long position = 0;
-        bool foundPosition = false;
-        bool nextJPG = false;
-    
-        int values[BLOCK_SIZE];
-    
-        // Goes over the file until the correct bytes are found
-        do{
-            fread(&buffer, BLOCK_SIZE , 1, inptr);
-        
-            values[BLOCK_SIZE] = *buffer;
+        if (fread(&buffer, BLOCK_SIZE , 1, inptr) == 1){
             
-            if (values[position] == FIRST_BYTE){
-            
-                if (values[position + 1] == SECOND_BYTE){
-                
-                    if (values[position + 2] == THIRD_BYTE){
-                    
-                        switch (values[position + 3]){
+            // Check for first few bytes of a jpg
+            if (buffer[0] == FIRST_BYTE){
+                if (buffer[1] == SECOND_BYTE){
+                    if (buffer[2] == THIRD_BYTE){
+                        switch (buffer[3]){
                             case 0xe0:
                             case 0xe1:
                             case 0xe2:
@@ -77,33 +69,31 @@ int main(int argc, char* argv[])
                             case 0xed:
                             case 0xee:
                             case 0xef:
-                                foundPosition = true;
+                                newJPG = true;
+                                startJPG = true;
                                 break;
                         }
                     }
                 }
             }
-            else
-                position++;
-        } while (foundPosition == false);
         
-        // Creates the jpg files
-        do {
-            char *title = NULL;
-            // Prints the name with the correct format
-            if (currentJPEG < 10)
-                sprintf(title, "00%d.jpg", currentJPEG);
-            else
-                sprintf(title, "0%d.jpg", currentJPEG);
-            FILE* img = fopen(title, "a");
-            
-            fwrite(&buffer, BLOCK_SIZE, 1, img);
-            fread(&buffer, BLOCK_SIZE , 1, inptr);
-            
-            if 
-            
-        } while(nextJPG == false);
+            if (startJPG){
+                // Creates the jpg files
+                if (newJPG){
+                    char title[8];
+                    // Prints the name with the correct format
+                    sprintf(title, "%03d.jpg", currentJPEG);
+                
+                    img = fopen(title, "a");
+                    currentJPEG++;
+                }
+                
+                fwrite(&buffer, BLOCK_SIZE, 1, img);
+            }
+        }
+        else{
+            fclose(inptr);
+            fclose(img);
+        }
     }
-    
-    free(buffer);
 }
